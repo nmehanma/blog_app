265ac19cf96943404623cb5e0e6cf794f5d705b5
nmehanma <nmehanma@gmail.com>
2020-11-12 01:15:30 -0500

    debugging

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -66,51 +66,51 @@ RSpec.describe "Articles", type: :request do
   end
 
 
-  describe "DELETE /articles/:id" do
-      context "with non-signed in user" do
-        before { delete "/articles/#{@article.id}" }
-  
-        it "redirects to the sign in page" do
-          expect(response.status).to eq 302
-          flash_message = "You need to sign in or sign up before continuing."
-          expect(flash[:alert]).to eq flash_message
-        end
-      end
-      
-      context "with signed in user who is non-owner" do
-        before do
-          login_as(@hannah)
-          delete "/articles/#{@article.id}"
-        end
-  
-        it "redirects to the home page" do
-          expect(response.status).to eq 302
-          flash_message = "You can only delete your own article."
-          expect(flash[:alert]).to eq flash_message
-        end
-      end
-  
-      context "with signed in user as owner successful delete" do
-        before do
-          login_as(@john)
-          delete "/articles/#{@article.id}"
-        end
-  
-        it "successfully deletes article" do
-          expect(response.status).to eq 200
-        end
+describe "DELETE /articles/:id" do
+    context "with non-signed in user" do
+      before { delete "/articles/#{@article.id}" }
+ 
+      it "redirects to the sign in page" do
+        expect(response.status).to eq 302
+        flash_message = "You need to sign in or sign up before continuing."
+        expect(flash[:alert]).to eq flash_message
       end
     end
-  
-  def destroy
-      unless @article.user == current_user
-        flash[:danger] = "You can only delete your own article"
-        redirect_to root_path  
-      else
-        if @article.destroy
-          flash[:success] = "Article has been deleted"
-          redirect_to articles_path
-        end
+    
+    context "with signed in user who is non-owner" do
+      before do
+        login_as(@hannah)
+        delete "/articles/#{@article.id}"
+      end
+ 
+      it "redirects to the home page" do
+        expect(response.status).to eq 302
+        flash_message = "You can only delete your own article."
+        expect(flash[:alert]).to eq flash_message
+      end
+    end
+ 
+    context "with signed in user as owner successful delete" do
+      before do
+        login_as(@john)
+        delete "/articles/#{@article.id}"
+      end
+ 
+      it "successfully deletes article" do
+        expect(response.status).to eq 200
       end
     end
   end
+ 
+def destroy
+    unless @article.user == current_user
+      flash[:danger] = "You can only delete your own article"
+      redirect_to root_path  
+    else
+      if @article.destroy
+        flash[:success] = "Article has been deleted"
+        redirect_to articles_path
+      end
+    end
+  end
+end
